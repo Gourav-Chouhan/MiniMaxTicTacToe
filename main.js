aaab8eae7f16b1841c04e17e1a557cfc2c3805e0
Gourav Chouhan <gg0uravgch0uhan@gmail.com>
2022-11-27 17:18:27 +0530

    pwa added

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 // let board = [];
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () {
+		navigator.serviceWorker
+			.register("/serviceWorker.js")
+			.then((res) => console.log("service worker registered"))
+			.catch((err) => console.log("service worker not registered", err));
+	});
+}
 let board = [
 	["X", " ", " "],
 	[" ", " ", " "],
